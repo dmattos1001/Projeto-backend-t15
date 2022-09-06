416338a597608196228c8393cc40f3a47248e8cb
Vinicius Shiryu Hagio <viniciusshiryu@outlook.comgit commit -m Primeiro>
2022-09-06 20:32:09 -0300

    feat: product entries routes

--- src/app.ts
+++ src/app.ts
@@ -3,12 +3,15 @@ import express from "express";
 import "express-async-errors";
 import { handleErrorMiddleware } from "./middlewares/handleError.middleware";
 import { providerRouter } from "./routes/provider/provider.routes";
+import productEntryRouter from "./routes/productEntry/productEntry.routes";
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use("/provider",providerRouter)
+app.use("/provider", providerRouter);
+
+app.use("/productentry", productEntryRouter);
 
 app.use(handleErrorMiddleware);
 
